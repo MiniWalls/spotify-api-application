e6d7acab9344c5451f903882603ede7dccbd3ab4
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2022-12-16 23:46:43 +0200

    This reverts commit 455c3ec0c1f03ddb05954dd79d9d778c0fa2ba6e.

--- client/src/components/UserInfoDisplay.js
+++ client/src/components/UserInfoDisplay.js
@@ -1,10 +1,5 @@
-import Image from 'react-bootstrap/Image'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
 export default function UserInfoDisplay(props){
-/*     console.log(JSON.stringify(props)); */
+    console.log(JSON.stringify(props));
     if(props.user){
         return(
             <Container className="UserInfo" fluid>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,8 +2,6 @@ import UserInfoDisplay from "../components/UserInfoDisplay";
 import LogInDisplay from "../components/LogInDisplay";
 import axios from 'axios';
 import {useEffect, useState} from 'react';
-import Button from "react-bootstrap/Button";
-import CryptoJS from "crypto-js";
 
 export default function Home(){
     const [accessToken, setAccessToken] = useState(null);
@@ -17,6 +15,7 @@ export default function Home(){
         while ( e = r.exec(q)) {
            hashParams[e[1]] = decodeURIComponent(e[2]);
         }
+        //console.log(hashParams);
         return hashParams;
       }
 
@@ -32,28 +31,21 @@ export default function Home(){
         }
     })
     setAccessToken(response.data.access_token);
+    //console.log(response.data.access_token);
     }
 
     async function getUserInfo(){
-        if(sessionStorage.getItem("userData") == null){ 
-            const address = 'https://api.spotify.com/v1/me';
+    const address = 'https://api.spotify.com/v1/me';
 
-            const response = await axios({
-            method: "get",
-            url: address,
-            headers: {
-                'Authorization': 'Bearer ' + accessToken
-            }
-            })
-            setUserData(response);
-            const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(response), key).toString();
-            sessionStorage.setItem("userData", encryptedData);
-        } else {
-            const encryptedData = sessionStorage.getItem("userData");
-            const decryptedBytes = CryptoJS.AES.decrypt(encryptedData, key);
-            const decryptedData = JSON.parse(decryptedBytes.toString(CryptoJS.enc.Utf8));
-            setUserData(decryptedData);
+        const response = await axios({
+        method: "get",
+        url: address,
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
         }
+        })
+        console.log(response);
+        setUserData(response);
         document.getElementById('login').style.display = 'none';
         document.getElementById('loggedin').style.display = 'block';
     };
@@ -75,7 +67,6 @@ export default function Home(){
                 <LogInDisplay />
             </div>
             <div id="loggedin">
-                <Button onClick={() => getRefreshToken()}>Get refresh token</Button>
                 <UserInfoDisplay user={userData}/>
             </div>
         </>
